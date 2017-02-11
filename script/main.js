9c2a703f3e00836663eab7cb26de9bd7c18c87c8
Stefano <stefanogelsomino84@gmail.com>
2017-02-11 19:47:57 +0100

    Primitive Data Types done

--- script/main.js
+++ script/main.js
@@ -1,6 +1,14 @@
+/*JUST STARTED*/
+
+
 alert("Hello World!");
 confirm("I love JavaScript");
 prompt("Do you want to learn JavaScript?");
+
+
+/*COMMON OPERATORS*/
+
+
 2 + 2;
 /*
 4
@@ -8,4 +16,78 @@ prompt("Do you want to learn JavaScript?");
 3 * 2;
 /*
 6
+*/
+4 / 3;
+/*
+1.3333333333333333
+*/
+6 - 8;
+/*
+-2
+*/
+10%3;
+/*
+1
+*/
+(3 + 2) * 5;
+/*
+25
+*/
+3 + 2 * 5;
+/*
+13
+*/
+30 - 2 * 3;
+/*
+24
+*/
+(30 - 2) * 3;
+/*
+84
+*/
+
+
+/*CONSOLE*/
+
+
+console.log("Hello World!")
+console.warn("Warning!")
+console.error("Error!")
+
+
+/*PRIMITIVE DATA TYPES*/
+
+
+2 + "2";
+/*
+22
+*/
+"22";
+"Hello" + "World";
+/*
+HelloWorld
+*/
+"Hello" + " " + "World";
+/*
+Hello World
+*/
+10 > 2;
+/*
+true
+*/
+10 < 2;
+/*
+false
+*/
+typeof(10 < 2);
+/*
+boolean
+*/
+typeof(2 + 2);
+/*
+number
+*/
+typeof(2 + "2");
+/*
+string
 */
